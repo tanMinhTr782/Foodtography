a44591ad25c521f98dcddafdd92b76c30a179015
levanbang376 <levanbang376@gmail.com>
2023-12-31 23:10:14 +0700

    [Feature] Integrate api create recipe

--- src/Screens/CreateRecipe/CreateRecipe.tsx
+++ src/Screens/CreateRecipe/CreateRecipe.tsx
@@ -11,6 +11,7 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { ImagePicker } from 'expo-image-picker'
 
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { RootScreens } from '..';
@@ -37,6 +38,21 @@ export const CreateRecipe = (props: CreateRecipeProps) => {
   //     count: "2",
   //   },
   // ]);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    }
+  };
 
   const removeIngredient = (name: String) => {
     setIngredients(ingredients.filter((item) => item.name !== name));
